f8fbe955962414ab1b499f762fb268e6cf77255c
gabrielgom3s <144627181+gabrielgom3s@users.noreply.github.com>
2026-04-29 20:21:46 -0300

    first commit

---
+++ src/main/java/dev/gwenterprice/CadastrosDeBrechos/CadastrosDeBrechosApplication.java
@@ -0,0 +1,13 @@
+package dev.gwenterprice.CadastrosDeBrechos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrosDeBrechosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrosDeBrechosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gwenterprice/CadastrosDeBrechos/CadastrosDeBrechosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gwenterprice.CadastrosDeBrechos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrosDeBrechosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
