314075f43744cbe1c0f182fa012789e8ef9efb47
gabrielgom3s <144627181+gabrielgom3s@users.noreply.github.com>
2026-04-30 20:21:25 -0300

    criado a controller

---
+++ src/main/java/dev/gwenterprice/CadastrosDeBrechos/Controller.java
@@ -0,0 +1,16 @@
+package dev.gwenterprice.CadastrosDeBrechos;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasvindas(){
+        return "Essa é minha primeira mensagem ";
+    }
+}
